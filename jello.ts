136eb36130c6f56a20c0b5a76bed3a1c07d338d0
son1228 <sonvipkl04@gmail.com>
2024-07-21 05:39:33 +0700

    [OBJECT] object type

--- jello.ts
+++ jello.ts
@@ -1,6 +1,14 @@
 // 1. ràng buộc kiểu dữ liệu
-console.log("ts");
 let number: Number = 3;
 let boolean: Boolean = true;
 let string: String = "Nguyen Van A";
-//
+// 2. object
+
+let user: {
+  fullName: String;
+  age: Number;
+} = {
+  fullName: "Nguyen Van A",
+  age: 18,
+};
+console.log(user);
--- js/jello.js
+++ js/jello.js
@@ -1,4 +1,8 @@
-console.log("ts");
 let number = 3;
 let boolean = true;
 let string = "Nguyen Van A";
+let user = {
+    fullName: "Nguyen Van A",
+    age: 18,
+};
+console.log(user);
